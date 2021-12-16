f6cb4ec8eb295a10cbe641d5b9dc8c68db48a340
laowang <laowang@qq.com>
2021-12-16 19:45:42 +0800

    three commit

--- src/main/java/com/wangkaiping/controller/studentController/LoginController.java
+++ src/main/java/com/wangkaiping/controller/studentController/LoginController.java
@@ -1,22 +1,28 @@
 package com.wangkaiping.controller.studentController;
-
+import com.wangkaiping.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 //登录控制器
 @Controller
 public class LoginController {
-    @RequestMapping("/login/")
+    @Autowired
+    private StudentService studentServiceImpl;
+
+
+
+    @RequestMapping("/login")
     public String loginBtn(Integer status){
         //用户登录
-        if(status==0) {
-            return "workbean/studentInterface/Home";
-        }else{
-            //管理员登录
-            return "workbean/managerInterface/Home";
-        }
+        return "workbean/studentInterface/Home";
 
     }
+
+
+
+
 
 }
